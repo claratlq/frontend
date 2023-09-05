b9c8bb5fd5f3ee901663c6acfed54f5fd36a24e3
test-dan-run <daniel.leongsj@gmail.com>
2023-09-05 07:04:44 +0000

    feat(auth): add google auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from "./App.jsx";
 import "./index.css";
-const GOOGLECLIENTID = process.env.GOOGLE_CLIENT_ID;
-const isDev = process.env.NODE_ENV !== "production";
+
+const GOOGLECLIENTID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const isDev = import.meta.NODE_ENV !== "production";
 const REACTWRAP = isDev ? React.Fragment : React.StrictMode;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
