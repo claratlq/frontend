1777e04cbb8812cc2329a40222b503b950e40e85
Jaywhisker <chengweixuan7@gmail.com>
2023-09-18 18:21:00 +0800

    Added PDF statuses (not completed, need to add functions)

---
+++ src/components/ChatContainer/ChatHeader/index.jsx
@@ -0,0 +1,17 @@
+import "../chatcontainerStyles.css"
+
+export default function ChatHeader({documents, history}) {
+
+    return (
+    <div className="sticky-header">
+        <p className="header-text">Welcome to AIDE, your work aid on eHab!</p>
+        {history.length >= 1 ? (
+            <button title="Clear Chat" className="clear-chat"> Clear Chat</button>
+        ) : (
+            <div></div>
+        )}
+    </div>
+
+
+  );
+}
