6a6a98ecb7a4b3806e1dd51fc312e8e96dad72f2
bobbysy <25177772+bobbysy@users.noreply.github.com>
2023-10-16 17:10:20 +0800

    fix: getInitials requesting for additional params

--- src/components/UserIcon/index.jsx
+++ src/components/UserIcon/index.jsx
@@ -1,34 +1,33 @@
-
-import "../../styles/ChatBubble.css"
+import "../../styles/ChatBubble.css";
 
 let firstName, lastName;
 
 const getInitials = (firstName) => {
-    return firstName.charAt(0).toUpperCase();
+  return firstName.charAt(0).toUpperCase();
 };
 
 const user = window.localStorage.getItem("user");
 
 if (!!user) {
-    const nameArr = user.split(" ");
-    if (nameArr.length < 2) {
-        firstName = nameArr[0];
-        lastName = firstName.split("").pop();
-    } else {
-        firstName = nameArr[0];
-        lastName = nameArr[1];
-    }
+  const nameArr = user.split(" ");
+  if (nameArr.length < 2) {
+    firstName = nameArr[0];
+    lastName = firstName.split("").pop();
+  } else {
+    firstName = nameArr[0];
+    lastName = nameArr[1];
+  }
 } else {
-    firstName = "U";
-    lastName = "S";
+  firstName = "U";
+  lastName = "S";
 }
 
 export default function UserIcon() {
-    return (
-      <div className="user-icon">
-        <a className="user-icon-content" title={user}>
-            {getInitials(firstName,lastName)}
-        </a>
-      </div>
-    );
+  return (
+    <div className="user-icon">
+      <a className="user-icon-content" title={user}>
+        {getInitials(firstName)}
+      </a>
+    </div>
+  );
 }
